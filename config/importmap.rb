23c0a175ccff28892dc4ade515ded199f51d70ce
Patricia Kihara <patricia.kihara@outlook.com>
2023-03-29 19:52:22 -0300

    feat: add action text

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.3/dist/sweetalert2.all.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
